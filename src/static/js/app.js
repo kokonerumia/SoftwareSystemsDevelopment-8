548d180c0b78b44378d3241057d79b6da20f24a8
koknerumia <kokonerumia@gmail.com>
2026-01-20 21:58:15 +0900

    Add header and Docker publish workflow

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -2,6 +2,11 @@ function App() {
     const { Container, Row, Col } = ReactBootstrap;
     return (
         <Container>
+            <Row className="mb-4">
+                <Col className="text-center">
+                    <h1>ToDo App by Masanao Iida</h1>
+                </Col>
+            </Row>
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
                     <TodoListCard />
